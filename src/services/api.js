b90b60cf76689a01b14f965c1c1ffdd2a2d1ec42
Yago Castro <yago.fanstour@gmail.com>
2022-11-15 17:10:49 -0300

    req 10 completo

--- src/services/api.js
+++ src/services/api.js
@@ -19,24 +19,54 @@ export async function getProductById(id) {
   return data;
 }
 
+// export function handleClick({ target }) {
+//   const bag = {
+//     name: target.title,
+//     image: target.name,
+//     price: target.id,
+//     numero: 1,
+//   };
+
+//   const emptyStorage = JSON.parse(localStorage.getItem('dataCart')) === null;
+//   const data = JSON.parse(localStorage.getItem('dataCart'));
+//   if (emptyStorage) {
+//     localStorage.setItem('dataCart', JSON.stringify([bag]));
+//   } else {
+//     JSON.parse(localStorage.getItem('dataCart'));
+//     const dataProducts = [...data, bag];
+//     localStorage.setItem('dataCart', JSON.stringify(dataProducts));
+//   }
+// }
+
+export function checkStorage() {
+  const emptyStorage = JSON.parse(localStorage.getItem('dataCart')) === null;
+  if (emptyStorage) {
+    localStorage.setItem('dataCart', JSON.stringify([]));
+  }
+  return JSON.parse(localStorage.getItem('dataCart'));
+}
+
 export function handleClick({ target }) {
+  const data = checkStorage();
   const bag = {
     name: target.title,
     image: target.name,
     price: target.id,
-    // qtd: 1,
+    numero: 1,
   };
-  const emptyStorage = JSON.parse(localStorage.getItem('dataCart')) === null;
-  if (emptyStorage) {
-    localStorage.setItem('dataCart', JSON.stringify([bag]));
+  const searchIndex = data.findIndex((item) => item.name === target.title);
+  if (searchIndex >= 0) {
+    data[searchIndex].numero += 1;
+    localStorage.setItem('dataCart', JSON.stringify(data));
   } else {
-    const data = JSON.parse(localStorage.getItem('dataCart'));
-    // if(data.includes(bag)) {
-
-    // }
-    // data.qtd += 1;
-    // console.log(bag);
     const dataProducts = [...data, bag];
     localStorage.setItem('dataCart', JSON.stringify(dataProducts));
   }
 }
+
+//   } else {
+// const data = JSON.parse(localStorage.getItem('dataCart'));
+// const dataProducts = [...data, bag];
+// localStorage.setItem('dataCart', JSON.stringify(dataProducts));
+//   }
+// }
